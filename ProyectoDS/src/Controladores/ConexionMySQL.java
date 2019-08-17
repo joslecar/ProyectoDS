9fdace8d081a40d0bcb0310b6d02652666964570
Jose <joslecar@espol.edu.ec>
2019-08-17 12:49:11 -0500

    Conexion

--- ProyectoDS/src/Controladores/ConexionMySQL.java
+++ ProyectoDS/src/Controladores/ConexionMySQL.java
@@ -18,39 +18,44 @@ import java.sql.SQLException;
 public class ConexionMySQL {
 
     // Librería de MySQL
-    public String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public String driver = "com.mysql.jdbc.Driver";
 
     // Nombre de la base de datos
-    public String database = "Sysapplog";
+    public String database = "databaseDS";
 
     // Host
-//<<<<<<< HEAD
-   //public String hostname = "169.254.127.21";
-//=======
-//>>>>>>> a837ff43041664401c12801dbb61fb286b8317c8*/
-    public String hostname = "SYSWEBSERVICE\\SQLEXPRESS";
+    public String hostname = "localhost";
 
-    // Puerto 1403
-    public String port = "1433";
+    // Puerto
+    public String port = "3306";
 
-    
-     // Nombre de usuario
-    public String username = "sa";
+    // Ruta de nuestra base de datos 
+    public String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=false";
+
+    // Nombre de usuario
+    public String username = "root";
 
     // Clave de usuario
-    
-    public String password = "Rootpass1";
-    
-    // Ruta de nuestra base de datos 
-    public String url = "jdbc:sqlserver://" + hostname + ":" + port + ";databaseName=" + database+";user="+username +";password="+password;
-
-   
+    public String password = "rootpass1";
     
     public Connection conn;
     //Metodo para iniciar la conexion
     public Connection conectarMySQL() {
         conn = null;
 
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return conn;
+    }
+    
+    public Connection conectarMySQL(String hostname, String database, String username, String password) {
+        conn = null;
+        String url = "jdbc:sqlserver://"+ hostname +":1433;databaseName="+database+";user="+username+";password="+password;
         try {
             Class.forName(driver);
             conn = DriverManager.getConnection(url, username, password);
