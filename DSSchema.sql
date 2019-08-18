197800f9c74ddaebf2d498b5bd44093839c6d5fd
Jose <joslecar@espol.edu.ec>
2019-08-17 23:26:26 -0500

    Ajustes en .sql

--- DSSchema.sql
+++ DSSchema.sql
@@ -1,8 +1,8 @@
-create database DSdatabase;
+#create database DSdatabase;
 
 use DSdatabase;
 
-create table Producto(IDProducto varchar(10) PRIMARY KEY, 
+create table if not exists Producto(IDProducto varchar(10) PRIMARY KEY, 
 					Categoria varchar(20), 
 					Nombre varchar(50),
                     Stock int,
@@ -10,14 +10,14 @@ create table Producto(IDProducto varchar(10) PRIMARY KEY,
                     Marca varchar(20),
                     PrecioCompra float,
                     PrecioVenta float);
-create table Cliente(IDCliente varchar(10) PRIMARY KEY,
+create table if not exists Cliente(IDCliente varchar(10) PRIMARY KEY,
 					Nombre varchar(100),
                     Apellido varchar(100));
                     
-create table TipoProducto(IDTipo varchar(10) PRIMARY KEY,
+create table if not exists TipoEmpleado(IDTipo varchar(10) PRIMARY KEY,
 						TipoEmpleado varchar(50));
 
-create table Empleado(IDEmpleado varchar(10) PRIMARY KEY,
+create table if not exists Empleado(IDEmpleado varchar(10) PRIMARY KEY,
 					Nombre varchar(100),
                     Apellido varchar(100),
 					FechaIngreso datetime,
@@ -25,65 +25,68 @@ create table Empleado(IDEmpleado varchar(10) PRIMARY KEY,
                     Tipo varchar(10),
                     usuario varchar(100),
                     pass varchar(100),
-                    foreign key (Tipo) references TipoProducto(IDTipo)
+                    foreign key (Tipo) references TipoEmpleado(IDTipo)
                     );
-create table TblLocal(IDLocal varchar(10) PRIMARY KEY,
+create table if not exists TblLocal(IDLocal varchar(10) PRIMARY KEY,
 					Ubicacion varchar(200),
                     Ciudad varchar(100),
                     Administrador varchar(10),
                     foreign key (Administrador) references Empleado(IDEmpleado)
                     );
-create table Bodega(IDBodega varchar(10) PRIMARY KEY,
+create table if not exists Bodega(IDBodega varchar(10) PRIMARY KEY,
 				IDAdministrador varchar(10),
                 Ubicacion varchar(200),
                 Ciudad varchar(100),
                 foreign key (IDAdministrador) references Empleado(IDEmpleado));
-create table InventarioBodega(IDCountProducBod int PRIMARY KEY,
+create table if not exists InventarioBodega(IDCountProducBod int PRIMARY KEY,
 								IDProducto varchar(10),
                                 IDBodega varchar(10),
                                 StockXBodega int,
                                 foreign key (IDProducto) references Producto(IDProducto),
                                 foreign key (IDBodega) references Bodega(IDBodega));
-create table InventarioLocal(IDCountProduct int PRIMARY KEY,
+create table if not exists InventarioLocal(IDCountProduct int PRIMARY KEY,
 							IDProducto varchar(10),
                             IDLocal varchar(10),
                             StockXLocal int,
                             foreign key (IDProducto) references Producto(IDProducto),
                             foreign key (IDLocal) references TblLocal(IDLocal));
-create table Cotizacion(IDCotizacion varchar(10) PRIMARY KEY,
+create table if not exists Cotizacion(IDCotizacion varchar(10) PRIMARY KEY,
 						IDEmpleado varchar(10),
                         IDCliente varchar(10),
                         Valor float,
                         Fecha DateTime,
                         foreign key (IDEmpleado) references Empleado(IDEmpleado),
                         foreign key (IDCliente) references Cliente(IDCliente));
-create table ProductosXCotizacion(IDCotizacion varchar(10),
+create table if not exists ProductosXCotizacion(IDCotizacion varchar(10),
 								IDProducto varchar(10),
                                 cantidadProduc int,
                                 PRIMARY KEY(IDCotizacion,IDProducto),
                                 foreign key (IDCotizacion) references Cotizacion(IDCotizacion),
                                 foreign key (IDProducto) references Producto(IDProducto));
-create table Pago(IDPago varchar(10) PRIMARY KEY,
+create table if not exists Pago(IDPago varchar(10) PRIMARY KEY,
 				Total float);
-create table PagoPayPal(IDPago varchar(10) PRIMARY KEY,
-						Cuenta varchar(30) PRIMARY KEY,
+create table if not exists PagoPayPal(IDPago varchar(10),
+						Cuenta varchar(30),
                         Detalle varchar(200),
                         Total float,
+                        PRIMARY KEY(IDPago,Cuenta),
                         foreign key (IDPago) references Pago(IDPago));
-create table PagoEfectivo(IDPago varchar(10) PRIMARY KEY,
+                        
+create table if not exists PagoEfectivo(IDPago varchar(10) PRIMARY KEY,
 						Fecha datetime,
                         Detalle varchar(200),
                         Total float,
                         foreign key (IDPago) references Pago(IDPago));
-create table PagoTarjeta(IDPago varchar(10) PRIMARY KEY,
-						IDTarjeta varchar(10) PRIMARY KEY,
+create table if not exists PagoTarjeta(IDPago varchar(10),
+						IDTarjeta varchar(10),
                         FechaExp varchar(7),
                         CVV varchar(3),
                         Detalle varchar(200),
                         Fecha Datetime,
                         Total float,
-                        foreign key (IDPago) references Pago(IDPago));
-create table Venta(IDVenta varchar(10) PRIMARY KEY,
+                        foreign key (IDPago) references Pago(IDPago),
+                        PRIMARY KEY(IDPago, IDTarjeta));
+create table if not exists Venta(IDVenta varchar(10) PRIMARY KEY,
 					IDEmpleado varchar(10),
                     IDCliente varchar(10),
                     IDPago varchar(10),
@@ -93,7 +96,7 @@ create table Venta(IDVenta varchar(10) PRIMARY KEY,
                     foreign key (IDCliente) references Cliente(IDCliente),
                     foreign key (IDPago) references Pago(IDPago)
                     );
-create table EnvioDomicilio(IDEnvio varchar(10) PRIMARY KEY,
+create table if not exists EnvioDomicilio(IDEnvio varchar(10) PRIMARY KEY,
 							IDVenta varchar(10),
                             IDCliente varchar(10),
                             IDRepartidor varchar(10),
@@ -101,23 +104,24 @@ create table EnvioDomicilio(IDEnvio varchar(10) PRIMARY KEY,
                             Fecha DateTime,
                             foreign key (IDVenta) references Venta(IDVenta),
                             foreign key (IDCliente) references Cliente(IDCliente),
-                            foreign key (IDRepartidor) references Empleado(IDCliente)
+                            foreign key (IDRepartidor) references Empleado(IDEmpleado)
                             );
-create table DocumentoElectronico(IDDocumento varchar(10) PRIMARY KEY,
+create table if not exists DocumentoElectronico(IDDocumento varchar(10) PRIMARY KEY,
 								IDVenta varchar(10),
                                 foreign key (IDVenta) references Venta(IDVenta));
-create table Factura(IDFactura varchar(10) PRIMARY KEY,
+create table if not exists Factura(IDFactura varchar(10) PRIMARY KEY,
 					IDDocumento varchar(10),
                     foreign key (IDDocumento) references DocumentoElectronico(IDDocumento));
-create table NotaVenta(IDNotaVenta varchar(10) PRIMARY KEY,
+create table if not exists NotaVenta(IDNotaVenta varchar(10) PRIMARY KEY,
 						IDDocumento varchar(10),
-                        foreign key (IDDocumento) references Documento(IDDocumento));
-create table ProductoXVenta(IDVenta varchar(10) PRIMARY KEY,
-							IDProducto varchar(10) PRIMARY KEY,
+                        foreign key (IDDocumento) references DocumentoElectronico(IDDocumento));
+create table if not exists ProductoXVenta(IDVenta varchar(10),
+							IDProducto varchar(10),
                             CantidadProduc int,
                             foreign key (IDVenta) references Venta(IDVenta),
-                            foreign key (IDProducto) references Producto(IDProducto));
-create table Reabastecimiento(IDEntrega varchar(10) PRIMARY KEY,
+                            foreign key (IDProducto) references Producto(IDProducto),
+                            PRIMARY KEY(IDVenta,IDProducto));
+create table if not exists Reabastecimiento(IDEntrega varchar(10) PRIMARY KEY,
 							IDProducto varchar(10),
                             IDRepartidor varchar(10),
                             IDLocal varchar(10),
@@ -127,9 +131,9 @@ create table Reabastecimiento(IDEntrega varchar(10) PRIMARY KEY,
                             foreign key (IDRepartidor) references Empleado(IDEmpleado),
                             foreign key (IDLocal) references TblLocal(IDLocal),
                             foreign key (SolicitadoPor) references Empleado(IDEmpleado));
-create table PagoRepartidor(IDPagoRep varchar(10),
+create table if not exists PagoRepartidor(IDPagoRep varchar(10) PRIMARY KEY,
 							IDRepartidor varchar(10),
                             Total float,
                             Fecha datetime,
-                            foreign key (IDRepartidor) references IDEmpleado(IDRepartidor));
+                            foreign key (IDRepartidor) references Empleado(IDEmpleado));
                     
---
+++ DSValues.sql
@@ -0,0 +1,32 @@
+insert into Producto values("PRDUC-001","Limpieza","PinoClean",20,"Unilevel",1.50,2.00),
+("PRDUC-002","Juegos","Playstation",20,"Sony",300.0,350.0),
+("PRDUC-003","Tecnologia","Core i7",20,"Microsoft",200.0,250.0),
+("PRDUC-004","Tecnologia","Mouse",20,"Genius",20.0,25.0);
+
+insert into cliente values("CLI-0001","Jose","Carrion"),
+("CLI-0002","Adriana","Murrieta"),
+("CLI-0003","Bryan","Santos");
+
+insert into TipoEmpleado values("VEND-1000","Vendedor"),
+("GER-1000","Gerente"),
+("ADMIN-100","Administrador"),
+("JFBOD-100","Jefe de Bodega");
+
+insert into Empleado values("0999657238","Leonardo","Pacheco",'2017-01-01','1998-20-02',"ADMIN-100","joslecar","joseleo123"),
+('0989657121','Sebastian','Carrion','2018-01-01','2001-04-04',"JFBOD-100","leonter258","sebas123");
+
+insert into TblLocal values("LOCAL-001","Vernaza Norte","Guayaquil","0999657238"),
+("LOCAL-002","Ceibos","Guayaquil","0999657238");
+
+insert into Bodega values("BOD0001","0989657121","Vernaza Norte","Guayaquil");
+
+insert into InventarioBodega values(1,"PRDUC-001","BOD0001",10),
+(2,"PRDUC-002","BOD0001",10),
+(3,"PRDUC-003","BOD0001",10),
+(4,"PRDUC-004","BOD0001",10);
+
+insert into InventarioLocal values(1,"PRDUC-001","LOCAL-001",10),
+(1,"PRDUC-001","LOCAL-001",10),
+(1,"PRDUC-001","LOCAL-001",10),
+(1,"PRDUC-001","LOCAL-001",10);
+
